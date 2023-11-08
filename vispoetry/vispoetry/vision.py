72ed367487e75583e9bc87c310f4eb87399d7f31
Kyle <kyle.tz.cui@gmail.com>
2023-11-08 00:20:21 -0800

    sheeesh

--- vispoetry/vispoetry/vision.py
+++ vispoetry/vispoetry/vision.py
@@ -1,85 +1,88 @@
 import os
 import time
+import json
 import requests
 import pyautogui
 import openai
 import base64
 
-OPENAI_KEY = 
-
-curr_time = time.strftime("%H%M%S", time.localtime())
-path = os.getcwd() + f'/screens/{curr_time}.png'
-ss = pyautogui.screenshot()
-ss.save(path)
-
-def encode_image(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
-    
-base64_image = encode_image(path)
-
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {OPENAI_KEY}"
-}
-
-payload = {
-    "model": "gpt-4-vision-preview",
-    "messages": [
-      {
-        "role": "user",
-        "content": [
-          {
-            "type": "text",
-            "text": "What's in this image? Do not try to explain what is in the image, just describe what is in it."
-          },
-          {
-            "type": "image_url",
-            "image_url": {
-              "url": f"data:image/jpeg;base64,{base64_image}"
-            }
-          }
-        ]
-      }
-    ],
-    "max_tokens": 300
-}
-
-response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-
-res = response.json()['choices'][0]['message']['content']
-
-with open("log.txt", "w") as text_file:
-    text_file.write(res)
-
-print(f"Taking screenshot at {path}")
-print("Screenshot description:")
-print(res)
-
-sys_msg = "Respond in a numbered list of simple and succinct instructions of how to complete the given task. The instructions should be basic computer inputs such as moving the mouse or hitting keys on the keyboard. Do not provide any unnecessary information such as alternative ways to complete the task, just provide each instruction precisely and succintly with each instruction as its own bullet point. For instructions involving clicking on the screen, separate the instruction into moving the mouse to the desired location and into left-clicking or right-clicking as separate bullet points."
-
-task = f"The current state of my computer is as follows: {res}. How can I go to amazon.com?"
-
-completion = openai.ChatCompletion.create(
-  model="gpt-4",
-  messages=[
-    {"role": "system", "content": sys_msg},
-    {"role": "user", "content": task}
-  ]
-)
-
-res = completion.choices[0].message
-
-print("Instructions for task: ")
-print(task)
-print(res)
-
-l = res["content"].split("\n")
+OPENAI_KEY = "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"
 
 pyautogui.FAILSAFE = True
 
 #Number of seconds to move to given location
 MOUSE_SPEED = 1
+
+def get_screenshot():
+    curr_time = time.strftime("%H%M%S", time.localtime())
+    path = os.getcwd() + f'/screens/{curr_time}.png'
+    print(f"Taking screenshot at {path}")
+    ss = pyautogui.screenshot()
+    ss.save(path)
+    return path
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+def vision_screenshot(img):
+    """Get the GPT-4V response describing what is in the given screenshot image"""
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPENAI_KEY}"
+    }
+
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+        {
+            "role": "user",
+            "content": [
+            {
+                "type": "text",
+                "text": "What's in this image? Do not try to explain what is in the image, just describe what is in it."
+            },
+            {
+                "type": "image_url",
+                "image_url": {
+                "url": f"data:image/jpeg;base64,{img}"
+                }
+            }
+            ]
+        }
+        ],
+        "max_tokens": 300
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    res = response.json()['choices'][0]['message']['content']
+    print("Screenshot description:")
+    print(res)
+    return res
+
+def get_instructions(screenshot_description, task):
+    """Get a list of the next three instructions for how to complete the task given the current state of the computer as a natural language description of the screenshot."""
+    sys_msg = """Respond with the next three instructions for how to continue completing the given task given the current state of my computer. Respond in a numbered list of three simple and succinct instructions of how to continue completing the given task. The instructions should be basic computer inputs such as moving the mouse or hitting keys on the keyboard. Do not provide any unnecessary information such as alternative ways to complete the task, just provide each instruction precisely and succintly with each instruction as its own bullet point. For instructions involving clicking on the screen, separate the instruction into moving the mouse to the desired location and into left-clicking or right-clicking as separate bullet points."""
+
+    user_prompt = f"The current state of my computer is as follows: {screenshot_description}. {task}"
+
+    completion = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[
+        {"role": "system", "content": sys_msg},
+        {"role": "user", "content": user_prompt}
+    ]
+    )
+
+    instructions = completion.choices[0].message
+
+    print("Next three instructions for task: ")
+    print(task)
+    print(instructions)
+
+    l = instructions["content"].split("\n")
+    return l
 
 def move_mouse(x, y):
     """Move the mouse to the given pixel coordinates"""
@@ -93,14 +96,14 @@ def mouse_click(click):
     pyautogui.click()
     return
 
-def type(word):
+def keyboard_type(word):
     """Type the given word"""
     print(f"Typing: {word}")
     pyautogui.typewrite(word)
     return
 
-def run_conversation():
-    messages = [{"role": "user", "content": l[0]}]
+def get_auto_commands(instructions):
+    messages = [{"role": "user", "content": instructions[0]}]
     tools = [
         {
             "type": "function",
@@ -126,7 +129,7 @@ def run_conversation():
             "type": "function",
             "function": {
                 "name": "mouse_click",
-                "description": "Left click the mouse."
+                "description": "Left click the mouse.",
                 "parameters": {
                     "type": "object",
                     "properties": {
@@ -141,8 +144,8 @@ def run_conversation():
         }, {
             "type": "function",
             "function": {
-                "name": "type",
-                "description": "Types the given string of characters on the keyboard."
+                "name": "keyboard_type",
+                "description": "Types the given string of characters on the keyboard.",
                 "parameters": {
                     "type": "object",
                     "properties": {
@@ -164,34 +167,43 @@ def run_conversation():
     )
     response_message = response.choices[0].message
     tool_calls = response_message.tool_calls
-    
+    messages.append(response_message)
+
     if tool_calls:
-        # Step 3: call the function
-        # Note: the JSON response may not always be valid; be sure to handle errors
         available_functions = {
             "move_mouse": move_mouse,
-        }  # only one function in this example, but you can have multiple
-        messages.append(response_message)  # extend conversation with assistant's reply
-        # Step 4: send the info for each function call and function response to the model
+            "mouse_click": mouse_click,
+            "type": keyboard_type
+        }
+
         for tool_call in tool_calls:
             function_name = tool_call.function.name
             function_to_call = available_functions[function_name]
             function_args = json.loads(tool_call.function.arguments)
-            function_response = function_to_call(
-                x=function_args.get("x"),
-                y=function_args.get("y"),
-            )
+            
             messages.append(
                 {
                     "tool_call_id": tool_call.id,
                     "role": "tool",
-                    "name": function_name,
-                    "content": function_response,
+                    "function_to_call": function_to_call,
+                    "function_args": function_args,
                 }
-            )  # extend conversation with function response
-        """second_response = openai.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
-            messages=messages,
-        )  # get a new response from the model where it can see the function response"""
-        return messages
-print(run_conversation())
+            )
+    return messages
+
+def pause(duration=3):
+    print("Sleeping...")
+    time.sleep(duration)
+
+ss_path = get_screenshot()
+base64_image = encode_image(ss_path)
+vision_res = vision_screenshot(base64_image)
+with open("log.txt", "w") as text_file:
+    text_file.write(vision_res)
+pause()
+
+task = "How can I go to amazon.com?"
+instructs = get_instructions(vision_res, task)
+pause()
+
+print(get_auto_commands(instructs))
